a1296d4e95930fd499385d0ff9fb51dda09cb378
Javier <jr.14.ushio@gmail.com>
2017-10-14 12:09:48 -0500

    Initial Commit

--- app/controllers/mainController.php
+++ app/controllers/mainController.php
@@ -5,7 +5,7 @@ class mainController
 
   public function index()
   {
-    $user = User::find(2);
+    $user = 'User::find(2)';
     Response::render("home", ['user' => $user,'ruta' => dirname(__DIR__)]);
   }
   public function about()
