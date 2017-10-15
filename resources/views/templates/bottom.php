895225f2d76d30ab9703008ec17a3feedab35e3a
Javier <jr.14.ushio@gmail.com>
2017-10-15 04:10:48 -0500

    Initial Commit

---
+++ resources/views/templates/bottom.php
@@ -0,0 +1,22 @@
+
+<script src="../../public/js/jquery-2.2.4.min.js" charset="utf-8"></script>
+<script src="../../public/js/bootstrap.min.js" charset="utf-8"></script>
+
+<script src="../../public/js/script.js" charset="utf-8"></script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.10&appId=2403383113219415";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+
+
+
+</body>
+</html>
